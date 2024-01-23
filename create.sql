070bc636cb1d4524daad72bd730a5b0f87f7a373
ThisAster <freizen034@gmail.com>
2024-01-23 16:53:07 +0300

    edit

--- create.sql
+++ create.sql
@@ -1,15 +1,15 @@
 CREATE TYPE product_status AS ENUM('in stock', 'out of stock');
-CREATE TYPE shop_status as ENUM('opened', 'closed');
-CREATE TYPE sex as ENUM('male', 'female');
-CREATE TYPE job_state as ENUM('employed', 'unemployed');
-CREATE TYPE cruelty as ENUM('happy', 'modest', 'angry');
-CREATE TYPE room_state as ENUM('opened', 'closed');
-CREATE TYPE place_status as ENUM('opened', 'closed');
+CREATE TYPE shop_status AS ENUM('opened', 'closed');
+CREATE TYPE sex AS ENUM('male', 'female');
+CREATE TYPE job_state AS ENUM('employed', 'unemployed');
+CREATE TYPE cruelty AS ENUM('happy', 'modest', 'angry');
+CREATE TYPE room_state AS ENUM('opened', 'closed');
+CREATE TYPE place_status AS ENUM('opened', 'closed');
 
 CREATE TABLE IF NOT EXISTS Product
 (
     id SERIAL PRIMARY KEY,
-    product_name  VARCHAR(255) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
     product_state product_status,
     product_price INTEGER
 );
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Product
 CREATE TABLE IF NOT EXISTS Shop
 (
     id SERIAL PRIMARY KEY,
-    shop_status ENUM ('opened', 'closed')
+    shop_status shop_status
 );
 
 CREATE TABLE IF NOT EXISTS Product_Range
@@ -86,23 +86,36 @@ CREATE TABLE IF NOT EXISTS Room
     room_status room_state
 );
 
+CREATE TABLE IF NOT EXISTS Place
+(
+    id SERIAL PRIMARY KEY,
+    address_id INTEGER REFERENCES Address (id) ON DELETE CASCADE NOT NULL,
+    owner_id INTEGER REFERENCES Owner (id) ON DELETE CASCADE NOT NULL,
+    admin_id INTEGER REFERENCES Admin (id) ON DELETE CASCADE NOT NULL,
+    master_id INTEGER REFERENCES Master (id) ON DELETE CASCADE NOT NULL,
+    place_state place_status,
+    relax_room_id INTEGER REFERENCES Relax_Room (id) ON DELETE CASCADE NOT NULL,
+    shop_id INTEGER REFERENCES Shop (id) ON DELETE CASCADE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Event_Time
 (
     id SERIAL PRIMARY KEY,
     start TIME WITHOUT TIME ZONE,
     finish TIME WITHOUT TIME ZONE,
-    start_date date,
-    finish_date date
+    start_date DATE,
+    finish_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Event
 (
     id SERIAL PRIMARY KEY,
-    event_time_id INTEGER REFERENCES Event_time (id) ON DELETE CASCADE NOT NULL
+    event_time_id INTEGER REFERENCES Event_Time (id) ON DELETE CASCADE NOT NULL,
     event_name VARCHAR(255),
     event_status VARCHAR(20)
 );
 
+-- Assuming Address table is created before referencing in Equipment
 CREATE TABLE IF NOT EXISTS Equipment
 (
     id SERIAL PRIMARY KEY,
@@ -118,26 +131,14 @@ CREATE TABLE IF NOT EXISTS Buyer
     place_id INTEGER REFERENCES Place (id) ON DELETE CASCADE NOT NULL,
     ban_status BOOLEAN NOT NULL,
     warning_count INTEGER,
-    role_duty_id INTEGER REFERENCES Role_duty (id) ON DELETE CASCADE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Place
-(
-    id SERIAL PRIMARY KEY,
-    address_id INTEGER REFERENCES Address (id) ON DELETE CASCADE NOT NULL,
-    owner_id INTEGER REFERENCES Owner (id) ON DELETE CASCADE NOT NULL,
-    admin_id INTEGER REFERENCES Admin (id) ON DELETE CASCADE NOT NULL,
-    master_id INTEGER REFERENCES Master (id) ON DELETE CASCADE NOT NULL,
-    place_state place_status,
-    relax_room_id INTEGER REFERENCES Relax_room (id) ON DELETE CASCADE NOT NULL,
-    shop_id INTEGER REFERENCES Shop (id) ON DELETE CASCADE NOT NULL
+    role_duty_id INTEGER REFERENCES Role_Duty (id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Place_to_Event
 (
     id SERIAL PRIMARY KEY,
     place_id INTEGER REFERENCES Place (id) ON DELETE CASCADE NOT NULL,
-    event_id INTEGER REFERENCES Event (id) ON DELETE CASCADE NOT NULL,
+    event_id INTEGER REFERENCES Event (id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Place_to_Buyer
