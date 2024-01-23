64bf31a0750d920d04808b2268cd13aafd34628f
ThisAster <freizen034@gmail.com>
2024-01-23 16:33:37 +0300

    edit

--- create.sql
+++ create.sql
@@ -1,24 +1,23 @@
+CREATE TYPE product_status AS ENUM('in stock', 'out of stock');
+CREATE TYPE shop_status as ENUM('opened', 'closed');
+CREATE TYPE sex as ENUM('male', 'female');
+CREATE TYPE job_state as ENUM('employed', 'unemployed');
+CREATE TYPE cruelty as ENUM('happy', 'modest', 'angry');
+CREATE TYPE room_state as ENUM('opened', 'closed');
+CREATE TYPE place_status as ENUM('opened', 'closed');
+
 CREATE TABLE IF NOT EXISTS Product
 (
     id SERIAL PRIMARY KEY,
     product_name  VARCHAR(255) NOT NULL,
-    product_status ENUM ('in stock', 'out of stock'),
+    product_state product_status,
     product_price INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Shop
 (
     id SERIAL PRIMARY KEY,
-    shop_status ENUM ('opened', 'closed')
-);
-
-CREATE TABLE IF NOT EXISTS Product_Range
-(
-    shop_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
-    CONSTRAINT shop_id_fk FOREIGN KEY (shop_id) REFERENCES Shop (id) ON DELETE CASCADE,
-    CONSTRAINT product_id_fk FOREIGN KEY (product_id) REFERENCES Product (id) ON DELETE CASCADE,
-    CONSTRAINT shop_product_id PRIMARY KEY (shop_id, product_id)
+    shop_state shop_status
 );
 
 CREATE TABLE IF NOT EXISTS Human
@@ -26,9 +25,9 @@ CREATE TABLE IF NOT EXISTS Human
     id SERIAL PRIMARY KEY,
     human_name VARCHAR(100),
     human_age INTEGER,
-    sex ENUM ('male', 'female'),
+    sex_type sex,
     salary INTEGER,
-    job_state ENUM ('employed', 'unemployed')
+    job_status job_state
 );
 
 CREATE TABLE IF NOT EXISTS Role_Duty
@@ -60,30 +59,44 @@ CREATE TABLE IF NOT EXISTS Owner
 (
     id SERIAL PRIMARY KEY,
     human_id INTEGER REFERENCES Human (id) ON DELETE CASCADE NOT NULL,
-    cruelty ENUM ('happy', 'modest', 'angry'),
+    cruelty_type cruelty,
     role_duty_id INTEGER REFERENCES Role_Duty (id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Relax_Room
 (
     id SERIAL PRIMARY KEY,
-    room_state ENUM ('opened', 'closed')
+    room_status room_state
+);
+
+CREATE TABLE IF NOT EXISTS Product_Range
+(
+    shop_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    CONSTRAINT shop_id_fk FOREIGN KEY (shop_id) REFERENCES Shop (id) ON DELETE CASCADE,
+    CONSTRAINT product_id_fk FOREIGN KEY (product_id) REFERENCES Product (id) ON DELETE CASCADE,
+    CONSTRAINT shop_product_id PRIMARY KEY (shop_id, product_id)
 );
 
 CREATE TABLE IF NOT EXISTS Room
 (
     id SERIAL PRIMARY KEY,
     place_id INTEGER REFERENCES Place (id) ON DELETE CASCADE NOT NULL,
-    human_id INTEGER REFERENCES Human (id) ON DELETE CASCADE NOT NULL
+    human_id INTEGER REFERENCES Human (id) ON DELETE CASCADE NOT NULL,
+    room_status room_state
 );
 
 CREATE TABLE IF NOT EXISTS Event_Time
 (
     id SERIAL PRIMARY KEY,
     start TIME WITHOUT TIME ZONE,
-    finish TIME WITHOUT TIME ZONE,
-    start_date date,
-    finish_date date
+    finish TIME WITHOUT TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS Address
+(
+    id SERIAL PRIMARY KEY,
+    address_name VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS Event
@@ -92,12 +105,6 @@ CREATE TABLE IF NOT EXISTS Event
     event_time_id INTEGER REFERENCES Event_time (id) ON DELETE CASCADE NOT NULL
     event_name VARCHAR(255),
     event_status VARCHAR(20)
-);
-
-CREATE TABLE IF NOT EXISTS Address
-(
-    id SERIAL PRIMARY KEY,
-    address_name VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS Equipment
@@ -125,7 +132,7 @@ CREATE TABLE IF NOT EXISTS Place
     owner_id INTEGER REFERENCES Owner (id) ON DELETE CASCADE NOT NULL,
     admin_id INTEGER REFERENCES Admin (id) ON DELETE CASCADE NOT NULL,
     master_id INTEGER REFERENCES Master (id) ON DELETE CASCADE NOT NULL,
-    place_status ENUM ('opened', 'closed'),
+    place_state place_status,
     relax_room_id INTEGER REFERENCES Relax_room (id) ON DELETE CASCADE NOT NULL,
     shop_id INTEGER REFERENCES Shop (id) ON DELETE CASCADE NOT NULL
 );
