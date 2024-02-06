6f61cfe24eda470647dfd4aa86431ddd1709a3ab
ThisAster <freizen034@gmail.com>
2024-02-06 19:29:14 +0300

    edit

--- drop_index.sql
+++ drop_index.sql
@@ -2,5 +2,6 @@ DROP INDEX IF EXISTS idx_event_time_interval;
 
 DROP INDEX IF EXISTS idx_product_product_price;
 
-DROP INDEX IF EXISTS idx_admin_sold_count;
+DROP INDEX IF EXISTS idx_equipment_product_price;
 
+DROP INDEX IF EXISTS idx_master_not_fixed_and_fixed;
